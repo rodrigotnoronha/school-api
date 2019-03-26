a27a2858c48825683560fb2d1d8f16da4b7a2be7
Leandro Henrique <emtudo@gmail.com>
2019-03-26 02:03:07 -0300

    fix ;

--- app/Domains/Courses/Events/FrequencyCreating.php
+++ app/Domains/Courses/Events/FrequencyCreating.php
@@ -3,14 +3,13 @@
 namespace Emtudo\Domains\Courses\Events;
 
 use Emtudo\Domains\Courses\Frequency;
-use Illuminate\Queue\SerializesModels;
 
 class FrequencyCreating
 {
     public function __construct(Frequency $frequency)
     {
         if ($frequency->present) {
-            $frequency->justified_absence = false
+            $frequency->justified_absence = false;
         }
     }
 }
