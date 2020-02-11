29b246ca52a998ad6752fdf8b191066df9108881
rodrigotnoronha <rodrigotnoronha@gmail.com>
2020-02-11 13:37:39 -0300

    Ajusta para deploy

--- config/logging.php
+++ config/logging.php
@@ -11,7 +11,7 @@ return [
     | one of the channels defined in the "channels" configuration array.
     |
     */
-    'default' => env('LOG_CHANNEL', 'single'),
+    'default' => env('LOG_CHANNEL', 'stack'),
     /*
     |--------------------------------------------------------------------------
     | Log Channels
