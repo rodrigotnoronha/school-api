7d15fe59dde20525e58072c7ec84547c0d374337
rodrigotnoronha <rodrigotnoronha@gmail.com>
2020-02-13 16:18:38 -0300

    remove lib cors antiga

--- app/Units/HttpKernel.php
+++ app/Units/HttpKernel.php
@@ -15,7 +15,6 @@ class HttpKernel extends Kernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
